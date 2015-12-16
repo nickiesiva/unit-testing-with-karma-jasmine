e0a47e16b35cee8cf68976c69f5824e23fb2c007
nickiesiva <nickiesiva@gmail.com>
2015-12-16 20:44:23 +0700

    add controller documentation

--- app/calculator/calculator.controller.js
+++ app/calculator/calculator.controller.js
@@ -10,10 +10,24 @@ angular
     vm.sum = sum;
     vm.subtract = subtract; 
 
+    /**
+     * Sum two number
+     *
+     * @param int first_number
+     * @param int second_number
+     * @returns int sum result
+     */
     function sum(first_number, second_number){
       return first_number+second_number;
     }
 
+    /**
+     * Subtract two number
+     *
+     * @param int first_number
+     * @param int second_number
+     * @returns int subtract result
+   */
     function subtract(first_number, second_number){
       return first_number-second_number;
     }
